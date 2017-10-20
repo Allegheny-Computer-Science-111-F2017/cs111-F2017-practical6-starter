afb2ff08c3b4c6786da11a26de184d7539be346d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-19 23:16:18 -0400

    Adding the first version of the YearChecker file.

---
+++ src/main/java/practicalsix/YearChecker.java
@@ -0,0 +1,28 @@
+package practicalsix;
+
+public class YearChecker {
+
+  int year;
+
+  private static final int REFERENCE_YEAR = 2013;
+
+  public YearChecker(int startingYear) {
+    year = startingYear;
+  }
+
+  public boolean isLeapYear() {
+    boolean isLeapYear = false;
+    return isLeapYear;
+  }
+
+  public boolean isCicadaYear() {
+    boolean isCicadaYear = false;
+    return isCicadaYear;
+  }
+
+  public boolean isSunspotYear() {
+    boolean isSunspotYear = false;
+    return isSunspotYear;
+  }
+
+}
